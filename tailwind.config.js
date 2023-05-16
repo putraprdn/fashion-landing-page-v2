e75ce7ecdfee44d1cf41d0d9c10f080e1d48592a
putraprdn <80741806+putraprdn@users.noreply.github.com>
2023-05-16 14:30:54 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '6.25rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
